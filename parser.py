1eb47e689837b9f0119f41d4a79d20f72210a7cf
Sergey Katkov <s.katkov@innopolis.university>
2024-08-01 18:48:51 +0200

    first commit

---
+++ parser.py
@@ -0,0 +1,83 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+import datetime
+import os
+
+
+with open('links_DO_NOT_CHANGE_FILE_TITLE.txt', 'r') as f:
+    links_list = [line.strip() for line in f.readlines()]
+
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
+    'Referer': 'https://google.com',
+}
+
+
+result = list()
+
+for page in links_list:
+    response = requests.get(page, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    if response.status_code != requests.codes.ok:
+        print(f'Проблема со страницой {page}./nКод ошибки: {response.status_code}')
+        continue
+
+    # Ищем блок с товарами
+    if soup.find(class_='product-thumb'):
+        for good in soup.find_all(class_='product-thumb'):
+            title_img = title_caption = href_caption = ''
+            if good.find(class_='img-responsive'):
+                title_img = good.find(class_='img-responsive').get('alt')
+            if good.find(class_='caption'):
+                elem = good.find(class_='caption')
+                title_caption = elem.find('a').text.strip()
+                href = elem.find('a').get('href')
+            if good.find(class_='price'):
+                price = good.find(class_='price').text.strip()
+            title = title_img if len(title_img) > len(title_caption) else title_caption
+            result.append([title, price, href])
+    else:
+        print(f'Страница без товаров: {page}')
+
+    # Ищем другие категории
+    if soup.find(class_='catpr2'):
+        for tag in soup.find_all(class_='catpr2'):
+            link = tag.get('href')
+            if link not in links_list:
+                links_list.append(link)
+
+    # Ищем блок со следующими страницами этой же категории
+    if soup.find(class_='pagination'):
+        pagination = soup.find(class_='pagination').find(class_='active')
+        if pagination.next_sibling:
+            link = pagination.next_sibling.find('a').get('href')
+            if link not in links_list:
+                links_list.append(link)
+
+print("jopa")
+print(result)
+
+# field names 
+fields = ['Titile', 'Price', 'Link'] 
+
+# data rows of csv file 
+rows = result
+
+with open(f'parse.csv', 'w', encoding='utf-8') as f:
+	
+	# using csv.writer method from CSV package
+	write = csv.writer(f)
+	# write.writerow(fields)
+	write.writerows(result)
+
+
+
+# структура 
+# |- .exe
+# |- ссылки_НЕ_МЕНЯТЬ_НАЗВАНИЕ.txt
+# |- парсы
+#    |- парс_1
+#    |- парс_2
